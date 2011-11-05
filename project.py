00f2ef97e766d6cfa46d8efaafe587104944c6a2
Bruno Renié <buburno@gmail.com>
2011-11-05 12:29:44 +0100

    Fixed multiplication and added a test

--- project.py
+++ project.py
@@ -5,10 +5,16 @@ A test project for CI testing.
 def add(a, b):
     return a + b
 
+
 def multiply(a, b):
-    return a + b
+    return a * b
 
 
 def test_add():
     assert add(1, 5) == 6
     assert add(12, 12) == 24
+
+
+def test_multiply():
+    assert multiply(3, 4) == 12
+    assert multiply(-1, 5) == -5
